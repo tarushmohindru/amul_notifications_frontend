192523fcd535cee6b600664ce891316cb4cd70ae
Tarush Mohindru <tarushmohindru1515@gmail.com>
2025-07-14 15:42:53 +0530

    Minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,13 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Amul Notifications',
+  description: 'Get notified about Amul protein products availability',
+  authors: [
+    {
+      name: "Tarush Mohindru"
+    }
+  ]
 }
 
 export default function RootLayout({
